388c91cecd455aef261497d24701df3c0b254a2d
Alex Ilyaev <alexi@mitiga.io>
2023-03-19 22:16:02 +0200

    Update Prettier config options to match the defaults of 2.x
    - Sort the options alphabetically

--- prettier.config.js
+++ prettier.config.js
@@ -4,18 +4,28 @@
 /**
  * @see
  * https://prettier.io/docs/en/options.html
+ *
+ * - These are the default values for Prettier 2.x
+ *   Except for `singleQuote`, because double quotes are not fun.
+ * - A project should aim to use this config without overrides.
+ *
+ * Special notes:
+ * - `printWidth` - Read this:
+ *   https://prettier.io/docs/en/options.html#print-width
  */
 
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSpacing: true,
+  arrowParens: 'always',
   bracketSameLine: false,
-  endOfLine: 'auto',
+  bracketSpacing: true,
+  endOfLine: 'lf',
   htmlWhitespaceSensitivity: 'css',
   jsxSingleQuote: false,
   printWidth: 80,
   proseWrap: 'preserve',
+  quoteProps: 'as-needed',
   semi: true,
+  singleAttributePerLine: false,
   singleQuote: true, // default: false
   tabWidth: 2,
   trailingComma: 'es5',
